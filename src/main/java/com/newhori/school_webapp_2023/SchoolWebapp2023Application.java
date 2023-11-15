e1bcc0b60a3118d801a8627006cd33be4e9181b8
Vedant-07 <vedantnpatel7@gmail.com>
2023-11-15 11:33:13 +0530

    initailaized a new project

---
+++ src/main/java/com/newhori/school_webapp_2023/SchoolWebapp2023Application.java
@@ -0,0 +1,13 @@
+package com.newhori.school_webapp_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolWebapp2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolWebapp2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/newhori/school_webapp_2023/SchoolWebapp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newhori.school_webapp_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolWebapp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
