3243e6fd835638e51f0e9fe4116e5bee71c944c5
Vedant-07 <vedantnpatel7@gmail.com>
2023-11-16 16:46:21 +0530

    added static templates and assets ,reolve the problem of livereload

--- src/main/java/com/newhori/school_webapp_2023/SchoolWebapp2023Application.java
+++ src/main/java/com/newhori/school_webapp_2023/SchoolWebapp2023Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchoolWebapp2023Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SchoolWebapp2023Application.class, args);
 	}
 
